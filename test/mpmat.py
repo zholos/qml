6324e521ca18d3b9aab16d3abbda9e9a83d1fea8
Andrey Zholos <aaz@q-fu.com>
2015-01-28 13:20:41 +0000

    handle non-uniqueness of rank-deficient matrix factorization

--- test/mpmat.py
+++ test/mpmat.py
@@ -29,7 +29,12 @@ subjects = [
     random_matrix(7, 7),
     random_matrix(7, 4, sp.sqrt(2)),
     random_matrix(4, 7, sp.sqrt(3)),
-    Matrix([[1, 3], [2, 6]])
+    Matrix([[1, 3], [2, 6]]),
+    Matrix([[1, 3, 4], [2, 6, 8], [-3, -9, -12]]),
+    Matrix([[1, 3, 4], [2, 6, 8], [-3, -9, 12]]),
+    Matrix([[1,0,0],[1,1,0],[1,0,1],[0,1,1],[0,0,1]]) * hilbert_matrix(3, 5),
+    random_matrix(7, 3, sp.sqrt(2)) * sp.eye(3).row_join(sp.ones(3, 1)),
+    Matrix([[1,0],[0,1],[1,1],[1,-1]]) * random_matrix(2, 7, sp.sqrt(3))
 ]
 
 large_subjects = [
@@ -48,14 +53,15 @@ def test_msvd():
           not (.qml.mdim[u:usv 0]~2#d 0) and (.qml.mdim[v:usv 2]~2#d 1) and
             .qml.mdim[s:usv 1]~d:.qml.mdim x;::;
           not mortho[u] and mortho[v] and
-            all[0<=.qml.mdiag s] and mzero s _'til d 0;::;
+            all[0<=f:.qml.mdiag s] and mzero s _'til d 0;::;
           not mzero x-.qml.mm[u] .qml.mm[s] flip v;::;
-          b;1b;(p*/:m#/:u;m#m#/:s;(p:1-2*0>m#u 0)*/:(m:min d)#/:v)]};""")
+          b;1b;(p*/:m#/:u;m#m#/:s;
+              (p:(f>.qml.eps*f[0]*max d)*1-2*0>m#u 0)*/:(m:min d)#/:v)]};""")
 
     for A in subjects:
         U, S, V = map(mp.matrix, mp.svd(mp.matrix(A)))
         m = min(A.rows, A.cols)
-        p = mp.diag([mp.sign(u) for u in U[0, :]])
+        p = mp.diag([mp.sign(s * u) for s, u in zip(mp.chop(S), U[0, :])])
         U *= p
         S = mp.diag(S)
         V = V.T * p
