cc4d4ea370e3265c27ceafc4300440cb535ff963
Andrey Zholos <aaz@q-fu.com>
2015-03-10 15:58:17 +0000

    override xerbla() when using --build-lapack too

--- mk/src.mk
+++ mk/src.mk
@@ -32,7 +32,7 @@ qml.$(DLLEXT): $(OBJS) qml.symlist qml.mapfile
 
 qml.symlist: $(OBJS)
 	$(call nm_exports,$(OBJS)) | sed -n 's/^qml_/_&/p' >$@.tmp
-	$(if $(BUILD_LAPACK),,echo _xerbla_ >>$@.tmp)
+	$(if $(all $(BUILD_BLAS),$(BUILD_LAPACK)),,echo _xerbla_ >>$@.tmp)
 	mv $@.tmp $@
 
 qml.mapfile: qml.symlist
